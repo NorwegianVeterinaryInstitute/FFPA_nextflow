05f1b9792b748f6edb9fbd76362f01462f80fd93
Thomieh73 <Thhaverk@gmail.com>
2021-03-15 15:12:51 +0100

    removed options to add location of reads. Those have to be specified in the main.config file instead from now on

--- fullforce.sh
+++ fullforce.sh
@@ -3,19 +3,17 @@
 # This script is to make sure that the FFPA pipeline is run in a reproducible way
 # to run the script list the following command:
 #
-# ./fullforce.sh main.config location_of_illumina_reads location_of_nanopore_reads your_output_directory
+# ./fullforce.sh main.config your_output_directory
 
 script_directory=$(dirname ${BASH_SOURCE[0]})
 
 config=$1
-params.reads=$2
-params.longreads=$3
-outdir=$4
-workdir=${5:-$USERWORK/fullforce}
+outdir=$2
+workdir=${3:-$USERWORK/fullforce}
 
 DATE=($(date "+%Y%m%d_%R"))
 mkdir -p ${outdir}/config_files
 cp ${script_directory}/main.nf ${outdir}/config_files
 cp ${config} ${outdir}/config_files
 
-nextflow run ${script_directory}/main.nf -c ${config} --out_dir=${outdir} -work-dir ${workdir} -profile slurm,singularity -resume -with-report $DATE.report.html
+nextflow run ${script_directory}/main.nf -c ${config} --out_dir=${outdir} -work-dir ${workdir} -profile singularity -resume -with-report $DATE.report.html
