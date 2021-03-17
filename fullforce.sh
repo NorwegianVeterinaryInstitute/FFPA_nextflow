8d5379d7bdb207dac28b7b597710554b92e637fa
Thomieh73 <Thhaverk@gmail.com>
2021-03-17 12:12:57 +0100

    added a way to move the html reports from nextflow to the output directory specified

--- fullforce.sh
+++ fullforce.sh
@@ -11,9 +11,14 @@ config=$1
 outdir=$2
 workdir=${3:-$USERWORK/fullforce}
 
+# creating folder to collect nextflow html output files
+mkdir -p ${outdir}/nextflow_reports/
+
 DATE=($(date "+%Y%m%d_%R"))
 mkdir -p ${outdir}/config_files
 cp ${script_directory}/main.nf ${outdir}/config_files
 cp ${config} ${outdir}/config_files
 
 nextflow run ${script_directory}/main.nf -c ${config} --out_dir=${outdir} -work-dir ${workdir} -profile singularity -resume -with-report $DATE.report.html
+
+mv *.html ${outdir}/nextflow_reports/
